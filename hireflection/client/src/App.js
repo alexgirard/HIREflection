77a038e0ee1a57acb312f2256d395a82f107b592
Alexandra Girard <alexgirard999@gmail.com>
2018-09-15 14:31:38 -0400

    connecting in app.js

--- hireflection/client/src/App.js
+++ hireflection/client/src/App.js
@@ -4,10 +4,30 @@ import Title from "./components/Title";
 
 
 class App extends React.Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render(){
     return(
       <div>
-        <Title/>
+        <Title />
+        <p>{this.state.response}</p>
       </div>
     );
   }
