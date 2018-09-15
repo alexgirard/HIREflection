8049827068a6095248ca581e90d02b872648e4cb
aglao <aglao@edu.uwaterloo.ca>
2018-09-15 14:13:32 -0400

    Removed the multiple package.json's

--- hireflection/client/server.js
+++ hireflection/client/server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+  res.render(`./src/index.js`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
