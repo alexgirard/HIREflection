946b4d79cb6fd24db58fcc236b0d5ba0e811e0aa
Alexandra Girard <alexgirard999@gmail.com>
2018-09-15 11:51:52 -0400

    react single page app initial commit

---
+++ hireflection/src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <div>
+      <b>hire</b>flection
+    </div>
+    <h3 className="">Desc</h3>
+  </div>
+);
+
+export default Titles;
